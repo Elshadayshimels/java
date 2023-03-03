61ee4b50e938db555f10fc1fd6dcbbeb6ded0528
elshadayshimels <1elshaday2@gmail.com>
2023-03-03 10:42:59 -0800

    java test

---
+++ comparing/src/comparing/tt.java
@@ -0,0 +1,21 @@
+package comparing;
+
+public class tt {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num1 = 10;
+	      int num2 = 20;
+	      
+	      if(num1 == num2) {
+	         System.out.println("The two numbers are equal.");
+	      }
+	      else if(num1 > num2) {
+	         System.out.println("Number 1 is greater than number 2.");
+	      }
+	      else {
+	         System.out.println("Number 2 is greater than number 1.");
+
+	}
+
+	}}
